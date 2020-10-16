842f415c39a1b0677c5d0ea07f33a67bfb34d2dc
Guilherme <guidumasperes@gmail.com>
2020-10-16 17:46:47 -0300

    playing with jQuery and other stuff

---
+++ Curso Completo do Desenvolvedor Web/Project01/js/efeitos.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+	$("#logotipo").on("mouseover", function(){
+		$("#banner h1").addClass("efeito");
+	}).on("mouseout", function(){
+		$("#banner h1").removeClass("efeito");
+	});
+	$("#input-search").on("focus", function(){
+		$("li.search").addClass("active");
+	}).on("blur", function(){
+		$("li.search").removeClass("active");
+	});
+	$(".thumbnails").owlCarousel({
+		loop: true,
+		margin: 10,
+		responsive: {
+			0: {
+				items: 1
+			},
+			480: {
+				items: 3
+			},
+			1000: {
+				items: 4
+			}
+		}
+	});
+});
